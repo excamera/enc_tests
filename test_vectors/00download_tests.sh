7c05dcad46fea1476e45946b8853cac2cb1f0bb7
kwantam <kwantam@gmail.com>
2016-04-30 15:41:48 -0700

    grab subset1 test vectors from daala

--- test_vectors/00download_tests.sh
+++ test_vectors/00download_tests.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ ! -d "subset1-y4m" ]; then
+    wget -O "subset1-y4m.tar.gz" "https://people.xiph.org/~tterribe/daala/subset1-y4m.tar.gz"
+    # make sure the file we got is OK
+    echo "e72beed364ffbc3599f31971a21a749f9f3705ff36f2e3e96c4474f08dac9e39  subset1-y4m.tar.gz" | sha256sum -c -
+    if [[ "$?" == "0" ]]; then
+        tar xf subset1-y4m.tar.gz
+        rm subset1-y4m.tar.gz
+    else
+        exit 1
+    fi
+fi
+
 #if [ ! -f "ducks_take_off_short.y4m" ]; then
 #    wget "https://people.xiph.org/~tdaede/sets/video-1-short/ducks_take_off_short.y4m"
 #fi
